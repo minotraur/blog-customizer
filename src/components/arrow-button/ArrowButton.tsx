44b9a862e7a6c7533bc1a5a9f485f25434c70f21
Vlad Fedotov <fedotov85050@gmail.com>
2024-03-14 21:52:31 +0300

    Добавлена возможность открывать и закрывать сайдбар

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,30 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { FC } from 'react';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	onClick?: OnClick;
+	isOpen?: boolean;
+}
+
+export const ArrowButton: FC<ArrowButtonProps> = ({ onClick, isOpen }) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={`${styles.container} ${isOpen && styles.container_open}`}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={`${styles.arrow} ${isOpen && styles.arrow_open}`}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,84 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select/Select';
+import { RadioGroup } from '../radio-group/RadioGroup';
+import {
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Separator } from '../separator/Separator';
+import { useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [isFormOpen, setIsFormOpen] = useState(false);
+
+	const handleButtonClick = () => {
+		setIsFormOpen(!isFormOpen);
+	};
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton onClick={handleButtonClick} isOpen={isFormOpen} />
+			<aside
+				className={`${styles.container} ${
+					isFormOpen && styles.container_open
+				}`}>
 				<form className={styles.form}>
+					<Select
+						selected={defaultArticleState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={() => {
+							console.log('Изменение');
+						}}
+						onClose={() => {
+							console.log('Закрытие');
+						}}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='radioFonts'
+						options={fontSizeOptions}
+						selected={defaultArticleState.fontSizeOption}
+						title='размер шрифта'
+					/>
+					<Select
+						selected={defaultArticleState.fontColor}
+						options={fontColors}
+						onChange={() => {
+							console.log('Изменение');
+						}}
+						onClose={() => {
+							console.log('Закрытие');
+						}}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={defaultArticleState.backgroundColor}
+						options={backgroundColors}
+						onChange={() => {
+							console.log('Изменение');
+						}}
+						onClose={() => {
+							console.log('Закрытие');
+						}}
+						title='цвет фона'
+					/>
+					<Select
+						selected={defaultArticleState.contentWidth}
+						options={contentWidthArr}
+						onChange={() => {
+							console.log('Изменение');
+						}}
+						onClose={() => {
+							console.log('Закрытие');
+						}}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
