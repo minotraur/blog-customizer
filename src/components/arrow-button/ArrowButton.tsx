9a34fa09617a6b3dce343c3687ab816494b5c23b
Vladislav Fedotov <41328524+minotraur@users.noreply.github.com>
2024-03-15 18:08:15 +0300

    Задание выполнено
    * Добавлена возможность открывать и закрывать сайдбар
    
    * Добавлен функционал закрытия вне области сайдбара
    
    * Добавлен функционал изменения настроек в сайдбаре
    
    * Добавлен функционал изменения страницы
    
    * Незначительные исправления

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,34 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import { FC } from 'react';
+import cn from 'classnames';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	onClick?: OnClick;
+	isOpen?: boolean;
+}
+
+export const ArrowButton: FC<ArrowButtonProps> = ({ onClick, isOpen }) => {
+	const container = isOpen ? styles.container_open : styles.container;
+	const arrowImage = isOpen ? styles.arrow_open : styles.arrow;
+
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={cn(styles.container, container)}
+			onClick={onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={cn(styles.arrow, arrowImage)}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,15 +2,136 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { Select } from '../select/Select';
+import { RadioGroup } from '../radio-group/RadioGroup';
+import {
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { Separator } from '../separator/Separator';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
+import cn from 'classnames';
 
-export const ArticleParamsForm = () => {
+type ArticleStates = {
+	fontSelectState: OptionType;
+	setFontSelectState: Dispatch<SetStateAction<OptionType>>;
+
+	fontSizeSelectState: OptionType;
+	setFontSizeSelectState: Dispatch<SetStateAction<OptionType>>;
+
+	fontColorSelectState: OptionType;
+	setFontColorSelectState: Dispatch<SetStateAction<OptionType>>;
+
+	backgroundColorSelectState: OptionType;
+	setBackgroundColorSelectState: Dispatch<SetStateAction<OptionType>>;
+
+	contentWidthSelectState: OptionType;
+	setContentWidthSelectState: Dispatch<SetStateAction<OptionType>>;
+
+	onResetClick: () => void;
+	onSubmitClick: () => void;
+};
+
+export const ArticleParamsForm = (props: ArticleStates) => {
+	// Хуки
+	const [isFormOpen, setIsFormOpen] = useState(false);
+	const asideRef = useRef<HTMLElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				asideRef.current &&
+				!asideRef.current.contains(event.target as Node)
+			) {
+				setIsFormOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, []);
+
+	// Хэндлеры
+	const handleButtonClick = () => {
+		setIsFormOpen(!isFormOpen);
+	};
+
+	const handleFontSelectChange = (fontSelected: OptionType) => {
+		props.setFontSelectState(fontSelected);
+	};
+	const handleFontSizeSelectChange = (fontSizeSelected: OptionType) => {
+		props.setFontSizeSelectState(fontSizeSelected);
+	};
+	const handleFontColorSelectChange = (fontColorSelected: OptionType) => {
+		props.setFontColorSelectState(fontColorSelected);
+	};
+	const handleBackgroundColorSelectChange = (
+		backgroundColorSelected: OptionType
+	) => {
+		props.setBackgroundColorSelectState(backgroundColorSelected);
+	};
+	const handleContentWidthSelectChange = (contentWidthSelected: OptionType) => {
+		props.setContentWidthSelectState(contentWidthSelected);
+	};
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		props.onSubmitClick();
+	};
+
+	const container = isFormOpen ? styles.container_open : styles.container;
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={handleButtonClick} isOpen={isFormOpen} />
+			<aside className={cn(styles.container, container)} ref={asideRef}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Select
+						selected={props.fontSelectState}
+						options={fontFamilyOptions}
+						onChange={(fontSelected: OptionType) => {
+							handleFontSelectChange(fontSelected);
+						}}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						name='radioFonts'
+						options={fontSizeOptions}
+						selected={props.fontSizeSelectState}
+						title='размер шрифта'
+						onChange={handleFontSizeSelectChange}
+					/>
+					<Select
+						selected={props.fontColorSelectState}
+						options={fontColors}
+						onChange={handleFontColorSelectChange}
+						title='цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={props.backgroundColorSelectState}
+						options={backgroundColors}
+						onChange={handleBackgroundColorSelectChange}
+						title='цвет фона'
+					/>
+					<Select
+						selected={props.contentWidthSelectState}
+						options={contentWidthArr}
+						onChange={handleContentWidthSelectChange}
+						title='ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
+						<Button
+							title='Сбросить'
+							type='reset'
+							onClick={props.onResetClick}
+						/>
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
